8d7bdf066568e11e5616b1fae84d36da8a08b2e1
Naufal-farhan <nefener123@gmail.com>
2025-10-30 21:23:15 +0700

    Modifikasi Percobaan-4

--- NilaiKelompok20.java
+++ NilaiKelompok20.java
@@ -7,8 +7,12 @@ public class NilaiKelompok20 {
         int i, j, nilai;
         float totalNilai, rataNilai;
         i = 1;
+        float nTertinggi = 0;
+        float nTerendah = 100;
+        int kelompokTertinggi = 0;
+        int kelompokTerendah = 0;
 
-        while (i <= 6) {
+        while (i <= 5) {
             System.out.println("Kelompok "+i+"=====================================================================");
             totalNilai = 0;
             
@@ -19,8 +23,22 @@ public class NilaiKelompok20 {
             }
             rataNilai = totalNilai / 5;
             System.out.println("======= Rata-Rata nilai kelompok-"+i+" : "+rataNilai+"");
+            if(rataNilai>nTertinggi){
+                nTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+            if(rataNilai<nTerendah){
+                nTerendah = rataNilai;
+                kelompokTerendah = i;
+            }
             i++;
-
+            
         }
+        System.out.println();
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok-"+kelompokTertinggi);
+        System.out.println("Rata-rata nilai kelompok tertinggi adalah : "+nTertinggi);
+        System.out.println();
+        System.out.println("Kelompok dengan rata-rata Terendah adalah : "+kelompokTerendah);
+        System.out.println("Rata-rata nilai kelompok Terendah adalah : "+nTerendah);
     }
 }
